ebe000309f03e6725e182d389a474a1a50275d91
aminolevelrid <124892760+aminolevelrid@users.noreply.github.com>
2023-05-17 22:20:05 +0530

    Add files via upload

---
+++ add,subtract,multipication of matrics.c
@@ -0,0 +1,19 @@
+     int m,sum;
+     scanf("%d",&m);
+     int a[m],i,c[m];
+     for(i=0;i<m;i++)
+     scanf("%d",&a[i]);
+     for(i=0;i<m;i++)
+     printf("%d",a[i]);
+      scanf("%d ",&m);
+       int b[m];
+        for(i=0;i<m;i++)
+     scanf("%d",&b[i]);
+        for(i=0;i<m;i++)
+      printf("%d\n",b[i]);
+      for(i=0;i<m;i++)
+       for(i=0;i<m;i++){
+      c[m]=a[i]*b[i];
+        printf("%d",c[m]);
+           printf("\n");
+       }
