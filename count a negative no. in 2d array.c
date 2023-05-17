a2556fe50c1e823fcc84c13723f6a8107fc0fa72
aminolevelrid <124892760+aminolevelrid@users.noreply.github.com>
2023-05-17 22:21:01 +0530

    Add files via upload

---
+++ count a negative no. in 2d array.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int r,c;
+   printf("enter the value of r and c :");
+   scanf("%d%d",&r,&c);
+   int a[r][c],i,j,sum=0;
+   for(i=0;i<r;i++){
+       for(j=0;j<c;j++)
+       scanf("%d",&a[i][j]);
+   }
+    for(i=0;i<r;i++){
+       for(j=0;j<c;j++){
+       printf("%d ",a[i][j]);}
+       printf("\n");
+   }
+       for(i=0;i<r;i++){
+           for(j=0;j<c;j++){
+               if(a[i][j]<0)
+               sum++;
+           }
+       }
+       printf("%d",sum);
+  
+    return 0;
+}
